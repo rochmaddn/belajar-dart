26c3fb5bde31cf3449253d0d60432b27f68ff9c5
rochmaddn <rochmaddanufe@gmail.com>
2020-09-28 21:38:02 +0700

    Menambah print masukan input

--- standar_input.dart
+++ standar_input.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 main(List<String> args) {
   // membaca inputan string
+  print("Masukan Input");
   String input = stdin.readLineSync();
 
   // Jika inputan string hasilnya null
